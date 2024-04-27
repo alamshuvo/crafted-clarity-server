60f423681651d449927fca4571e56d753f0caed2
iftakhar <iftakharalamshuvo11@gmail.com>
2024-04-27 20:25:04 +0600

    single data and multiple data route get create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app=express();
@@ -11,8 +11,11 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-// Crafted-Clarity-Add-Craft
-// ROvlU8ENISCLEY6B
+
+// add craft item
+// app.get("/craft", async (req,res)=>{
+  
+// })
 
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.x9xlpou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,6 +33,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const craftCollection=client.db("craftDB").collection("craft")
+
+  
+  // all craft
+     app.get("/craft",async(req,res)=>{
+     const cursor=craftCollection.find();
+     const result = await cursor.toArray();
+     res.send(result);
+     })
+  // single craft iteam by id
+  app.get("/craft/:id",async(req,res)=>{
+    const id =req.params.id;
+    const quary={_id:new ObjectId(id)};
+    const result=await craftCollection.findOne(quary);
+    res.send(result)
+  })
+
+
+    app.post("/craft",async(req,res)=>{
+      const newCraft=req.body;
+      console.log(newCraft);
+      const result=await craftCollection.insertOne(newCraft)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
